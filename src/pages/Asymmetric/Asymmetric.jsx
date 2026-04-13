c42e3db39d1f071ad59d32c6abd025ea9ebc7b7b
Lê Thành Đạt <lethanhdat1567@gmail.com>
2026-04-13 15:10:13 +0700

    Test branch

--- src/pages/Asymmetric/Asymmetric.jsx
+++ src/pages/Asymmetric/Asymmetric.jsx
@@ -1,4 +1,5 @@
 function Asymmetric() {
+    // Test git
     // - Key Generation: Nút bấm để sinh cặp Public Key & Private Key ngẫu nhiên.
     // - Hiển thị: Phải show cả 2 Key lên màn hình (để người dùng copy).
     // - Encryption: Nhập Plaintext + Public Key -> Ciphertext.
